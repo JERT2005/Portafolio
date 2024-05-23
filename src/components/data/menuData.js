2961f9bf96213b223528feebd4e3ba6cfd000f42
Juan Torres <juanestebanrendontorres2005@gmail.com>
2024-05-23 14:01:24 -0500

    Portafolio

---
+++ src/components/data/menuData.js
@@ -0,0 +1,7 @@
+export const mainMenu = [
+     {link:"#", content:"INICIO", icon:"lucide:home"},
+     {link:"#about", content:"SOBRE MI", icon:"octicon:person-24"},
+     {link:"#portafolio", content:"PORTAFOLIO", icon:"carbon:portfolio"},
+     
+]
+
