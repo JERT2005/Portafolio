f046097ee2172455cd9bc56ac6586ba70a2ba404
Juan Torres <juanestebanrendontorres2005@gmail.com>
2024-05-23 15:44:13 -0500

    Error de MenuBlog solucionado

--- src/components/data/menuData.js
+++ src/components/data/menuData.js
@@ -1,7 +1,7 @@
 export const mainMenu = [
-     {link:"#", content:"INICIO", icon:"lucide:home"},
-     {link:"#about", content:"SOBRE MI", icon:"octicon:person-24"},
-     {link:"#portafolio", content:"PORTAFOLIO", icon:"carbon:portfolio"},
+     {link:"#", content:"Inicio", icon:"lucide:home"},
+     {link:"#about", content:"Experiencia", icon:"octicon:person-24"},
+     {link:"#portafolio", content:"Proyectos", icon:"carbon:portfolio"},
      
 ]
 
